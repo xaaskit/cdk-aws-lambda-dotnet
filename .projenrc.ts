379e8a8eff5da475f50c0cc71f2a64839403e4a4
Vincent Lesierse <vincent@lesierse.com>
2022-12-08 15:08:00 +0000

    Add NuGet package publishing

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,8 +2,9 @@ import { awscdk, JsonPatch } from 'projen';
 
 const project = new awscdk.AwsCdkConstructLibrary({
   projenrcTs: true,
-  name: '@xaaskit-cdk/aws-lambda-dotnet',
+  name: '@xaaskit-cdk/aws-lambda-dotnet',  
   description: 'AWS CDK construct library for .NET Lambda functions.',
+  majorVersion: 0,
   author: 'XaasKit',
   authorAddress: 'https://xaaskit.io',
   repositoryUrl: 'https://github.com/xaaskit/cdk-aws-lambda-dotnet.git',
@@ -11,7 +12,11 @@ const project = new awscdk.AwsCdkConstructLibrary({
   cdkVersion: '2.54.0',
   // deps: [],                /* Runtime dependencies of this module. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  packageName: '@xaaskit-cdk/aws-lambda-dotnet',
+  publishToNuget: {
+    packageId: 'XaasKit.CDK.AWS.Lambda.DotNet',
+    dotNetNamespace: 'XaasKit.CDK.AWS.Lambda.DotNet',
+  },
 });
 project.gitignore.addPatterns('/test/fixtures/**/bin', '/test/fixtures/**/obj');
 
